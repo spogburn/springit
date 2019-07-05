d86744d9d804ca50f5ee101af857a3918da57915
Stephanie Ogburn <stephanie.ogburn@gmail.com>
2019-07-05 16:25:27 -0600

    initial commit for spring boot reddit clone

---
+++ src/test/java/com/spo/springit/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spo.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
