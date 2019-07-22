6747f603d2dc842ed30506f86497fe8e5992032a
SPO <stephanie.ogburn@usda.gov>
2019-07-22 16:11:55 -0600

    entities

---
+++ src/main/java/com/spo/springit/model/Comment.java
@@ -0,0 +1,53 @@
+package com.spo.springit.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String body;
+
+    // link (belongs to)
+    public Comment() {
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment comment = (Comment) o;
+        return id.equals(comment.id) && body.equals(comment.body);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, body);
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" + "id=" + id + ", body='" + body + '\'' + '}';
+    }
+}
---
+++ src/main/java/com/spo/springit/model/Link.java
@@ -0,0 +1,64 @@
+package com.spo.springit.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Link {
+
+    private Long id;
+    private String title;
+    private String url;
+
+    /// comments (has many)
+
+    public Link() {
+    }
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "Link{" + "id=" + id + ", title='" + title + '\'' + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Link link = (Link) o;
+        return id.equals(link.id) && title.equals(link.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title);
+    }
+}
+
