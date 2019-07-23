a856c04b565bb1b428dfcd915576117d1d3b89fe
SPO <stephanie.ogburn@usda.gov>
2019-07-23 15:49:50 -0600

    created entities and refactored using Lombok

---
+++ src/main/java/com/spo/springit/domain/Link.java
@@ -0,0 +1,28 @@
+package com.spo.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Link {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NonNull
+    private String title;
+    @NonNull
+    private String url;
+
+    /// comments (has many)
+
+}
+
---
+++ src/main/java/com/spo/springit/domain/Vote.java
@@ -0,0 +1,24 @@
+package com.spo.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Vote {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private int vote;
+
+    // user
+    // link
+}
