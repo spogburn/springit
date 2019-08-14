db57391869c54d57c702c9c2474f5f9b68011d91
SPO <stephanie.ogburn@usda.gov>
2019-08-14 11:30:38 -0600

    added auditing

--- src/main/java/com/spo/springit/repository/VoteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spo.springit.repository;
-
-import com.spo.springit.domain.Vote;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VoteRepository extends JpaRepository<Vote, Long> {
-}
